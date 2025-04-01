43f73f0fa71fddf5a7da50739d28e65afba87bce
Radek Titěra <rodak.titera@gmail.com>
2025-04-01 10:54:21 +0200

    add: Boostrap colors

--- src/frontend/pages/LandingPage.tsx
+++ src/frontend/pages/LandingPage.tsx
@@ -1,30 +1,32 @@
-import * as React from "react";
-import {CoverImage, Preloader} from '../../libs/ui/components'; // UI components
-import {DecideImageSize} from '../../libs/utils';
-import { img } from "../../libs/types/size";
-import { BaseLayout } from "../../libs/ui/layouts";
+import * as React from 'react';
+import { CoverImage, Preloader } from '../../libs/ui/components'; // UI components
+import { DecideImageSize } from '../../libs/utils';
+import { img } from '../../libs/types/size';
+import { BaseLayout } from '../../libs/ui/layouts';
 
-const LandingPage : React.FC = () => {
-    const [size, setSize] = React.useState<img | null>(null);
+const LandingPage: React.FC = () => {
+  const [size, setSize] = React.useState<img | null>(null);
 
-    React.useEffect(() => {
-        setSize(DecideImageSize());
-    }, []);
+  React.useEffect(() => {
+    setSize(DecideImageSize());
+  }, []);
 
-    if (!size) return <Preloader></Preloader>; 
+  if (!size) return <Preloader></Preloader>;
 
-    return( 
-        <BaseLayout>
-            <div className="d-flex flex-row bg-black h-86-vh">
-                <div className="d-flex flex-column justify-content-end align-items-center overflow-hidden">
-                    <h2 className="text-white kanit-light text-left w-100 ms-5 mt-5 fs-10">Welcome to Modelab</h2>
-                    <div className="w-60 h-100 d-flex justify-content-center align-items-end">
-                        <CoverImage className="w-100 object-fit-contain ms-5 zoom-120" size={size}></CoverImage>
-                    </div>
-                </div>
-            </div>
-        </BaseLayout>
-    );
-}
+  return (
+    <BaseLayout>
+      <div className="d-flex flex-row bg-dark h-86-vh">
+        <div className="d-flex flex-column justify-content-end align-items-center overflow-hidden">
+          <h2 className="text-light kanit-light text-left w-100 ms-5 mt-5 fs-10">
+            Welcome to Modelab
+          </h2>
+          <div className="w-80 h-100 d-flex justify-content-center align-items-end">
+            <CoverImage className="w-100 object-fit-contain ms-5 zoom-120" size={size}></CoverImage>
+          </div>
+        </div>
+      </div>
+    </BaseLayout>
+  );
+};
 
 export default LandingPage;
--- src/libs/ui/components/Footer.tsx
+++ src/libs/ui/components/Footer.tsx
@@ -1,20 +1,21 @@
-import * as React from "react";
-import { cn } from "../../utils";
+import * as React from 'react';
+import { cn } from '../../utils';
 
-interface FooterProps{
-    className?: string;
-    children?:React.ReactNode[];
+interface FooterProps {
+  className?: string;
+  children?: React.ReactNode[];
 }
 
-export const Footer : React.FC<FooterProps> = (({className,children,...props}) => {
-    return(
-        <header
-            className={cn({className},"d-flex flex-row  justify-content-end align-items-center h-6-vh w-100 bg-white")}
-            {...props}
-        >
-            {children}
-        </header>
-    );
-})
-
-
+export const Footer: React.FC<FooterProps> = ({ className, children, ...props }) => {
+  return (
+    <header
+      className={cn(
+        { className },
+        'd-flex flex-row justify-content-end align-items-center h-6-vh w-100'
+      )}
+      {...props}
+    >
+      {children}
+    </header>
+  );
+};
