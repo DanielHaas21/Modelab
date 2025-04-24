a33b8df969c1c0477718dc06bd6f1c9620b9c0b3
Radek Titěra <rodak.titera@gmail.com>
2025-04-24 11:36:11 +0200

    add: BrowserResults

--- src/frontend/pages/Browser.tsx
+++ src/frontend/pages/Browser.tsx
@@ -1,12 +1,46 @@
 import * as React from 'react';
 import { BaseLayout } from '../../libs/ui/layouts';
-import { Input, ModelPreview, Preloader } from '../../libs/ui/components';
-import { BrowserFilters } from '../../libs/ui/components/BrowserFilters';
+import { Input, Preloader } from '../../libs/ui/components';
+import { BrowserFilters, CategoryOption, TagOption } from '../../libs/ui/components/BrowserFilters';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { BrowserResults, SearchQuery } from '../../libs/ui/components/BrowserResults';
 
 const Browser: React.FC = () => {
   const previewsCol = `col-xl-10 col-8`;
+
+  const [categories, setCategories] = React.useState<CategoryOption[]>([
+    { name: '3D Model', id: 1 },
+    { name: '2D Texture', id: 2 },
+    { name: 'Audio', id: 3 },
+  ]);
+  const [selectedCategory, setSelectedCategory] = React.useState<CategoryOption>(categories[0]);
+
+  const [tags, setTags] = React.useState<TagOption[]>([
+    { name: 'Medieval', id: 1, isSelected: true },
+    { name: 'C4D', id: 2, isSelected: true },
+    { name: 'Maya', id: 3 },
+    { name: 'Prop', id: 4 },
+    { name: 'FBX', id: 5 },
+    { name: 'Unity', id: 6 },
+    { name: 'Unity Second Test', id: 7 },
+  ]);
+
+  const [searchText, setSearchText] = React.useState<string>('');
+
+  const [searchQuery, setSearchQuery] = React.useState<SearchQuery>({
+    query: searchText,
+    category: selectedCategory,
+    tags: tags.filter((tag) => tag.isSelected),
+  });
+
+  React.useEffect(() => {
+    setSearchQuery({
+      query: searchText,
+      category: selectedCategory,
+      tags: tags.filter((tag) => tag.isSelected),
+    });
+  }, [searchText, tags, selectedCategory]);
 
   return (
     <BaseLayout bordered={true}>
@@ -19,6 +53,10 @@ const Browser: React.FC = () => {
               <Input
                 size="xl"
                 placeholder="Search"
+                value={searchText}
+                onChange={(event) => {
+                  setSearchText(event.target.value);
+                }}
                 inputGroupBefore={
                   <span className="input-group-text">
                     <FontAwesomeIcon icon={faMagnifyingGlass} className="fs-2" />
@@ -29,61 +67,15 @@ const Browser: React.FC = () => {
           </div>
           <div className="row w-100 h-100 pb-2" style={{ overflowY: 'scroll' }}>
             <section className={previewsCol + ' d-flex flex-wrap previews mx-0'}>
-              <React.Suspense fallback={<Preloader></Preloader>}>
-                <ModelPreview
-                  name="test"
-                  tags={[
-                    'test',
-                    'test',
-                    'test',
-                    'test',
-                    'test',
-                    'test',
-                    'test',
-                    'test',
-                    'test',
-                    'test',
-                    'test',
-                    'test',
-                    'test',
-                    'test',
-                    'test',
-                  ]}
-                ></ModelPreview>
-                <ModelPreview name="test"></ModelPreview>
-                <ModelPreview name="test"></ModelPreview>
-                <ModelPreview name="test"></ModelPreview>
-                <ModelPreview name="test"></ModelPreview>
-                <ModelPreview name="test"></ModelPreview>
-                <ModelPreview name="test"></ModelPreview>
-                <ModelPreview name="test"></ModelPreview>
-                <ModelPreview name="test"></ModelPreview>
-                <ModelPreview name="test"></ModelPreview>
-                <ModelPreview name="test"></ModelPreview>
-                <ModelPreview name="test"></ModelPreview>
-                <ModelPreview name="test"></ModelPreview>
-                <ModelPreview name="test"></ModelPreview>
-                <ModelPreview name="test"></ModelPreview>
-                <ModelPreview name="test"></ModelPreview>
-                <ModelPreview name="test"></ModelPreview>
-              </React.Suspense>
+              <BrowserResults searchQuery={searchQuery} />
             </section>
             <BrowserFilters
               className="sticky-top h-min-content"
-              categories={[
-                { name: '3D Model', id: 1 },
-                { name: '2D Texture', id: 2 },
-                { name: 'Audio', id: 3 },
-              ]}
-              tags={[
-                { name: 'Medieval', id: 1, isSelected: true },
-                { name: 'C4D', id: 2, isSelected: true },
-                { name: 'Maya', id: 3 },
-                { name: 'Prop', id: 4 },
-                { name: 'FBX', id: 5 },
-                { name: 'Unity', id: 6 },
-                { name: 'Unity Second Test', id: 7 },
-              ]}
+              categories={categories}
+              selectedCategory={selectedCategory}
+              setSelectedCategory={setSelectedCategory}
+              tags={tags}
+              setTags={setTags}
             />
           </div>
         </div>
---
+++ src/libs/ui/components/BrowserResults.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import { CategoryOption, TagOption } from './BrowserFilters';
+import { ModelPreview } from './ModelPreview';
+import { Label } from './Label';
+import { Preloader } from './Preloader';
+
+export interface SearchQuery {
+  category: CategoryOption;
+  tags: TagOption[];
+  query: string;
+}
+
+interface ModelResult {
+  name: string;
+  tags: string[];
+}
+
+interface BrowserResultProps {
+  searchQuery: SearchQuery;
+}
+
+const sleep = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const testResults: ModelResult[] = [
+  { name: 'test', tags: ['tag', 'tag', 'tag', 'tag', 'tag', 'tag'] },
+  { name: 'test', tags: ['tag', 'tag', 'tag', 'tag', 'tag', 'tag'] },
+  { name: 'test', tags: ['tag', 'tag', 'tag', 'tag', 'tag', 'tag'] },
+  { name: 'test', tags: ['tag', 'tag', 'tag', 'tag', 'tag', 'tag'] },
+  { name: 'test', tags: ['tag', 'tag', 'tag', 'tag', 'tag', 'tag'] },
+  { name: 'test', tags: ['tag', 'tag', 'tag', 'tag', 'tag', 'tag'] },
+  { name: 'test', tags: ['tag', 'tag', 'tag', 'tag', 'tag', 'tag'] },
+  { name: 'test', tags: ['tag', 'tag', 'tag', 'tag', 'tag', 'tag'] },
+  { name: 'test', tags: ['tag', 'tag', 'tag', 'tag', 'tag', 'tag'] },
+  { name: 'test', tags: ['tag', 'tag', 'tag', 'tag', 'tag', 'tag'] },
+  { name: 'test', tags: ['tag', 'tag', 'tag', 'tag', 'tag', 'tag'] },
+  { name: 'test', tags: ['tag', 'tag', 'tag', 'tag', 'tag', 'tag'] },
+  { name: 'test', tags: ['tag', 'tag', 'tag', 'tag', 'tag', 'tag'] },
+  { name: 'test', tags: ['tag', 'tag', 'tag', 'tag', 'tag', 'tag'] },
+  { name: 'test', tags: ['tag', 'tag', 'tag', 'tag', 'tag', 'tag'] },
+  { name: 'test', tags: ['tag', 'tag', 'tag', 'tag', 'tag', 'tag'] },
+  { name: 'test', tags: ['tag', 'tag', 'tag', 'tag', 'tag', 'tag'] },
+];
+
+const testFetch = async () => {
+  await sleep(500);
+  return testResults;
+};
+
+export const BrowserResults: React.FC<BrowserResultProps> = ({ searchQuery }) => {
+  const [loadedResults, setLoadedResults] = React.useState<ModelResult[]>([]);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const results = await testFetch();
+      setLoadedResults(results);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [searchQuery]);
+
+  if (loading) {
+    return <Preloader />;
+  }
+
+  if (loadedResults.length === 0) {
+    return <Label className="text-center w-100">No results</Label>;
+  }
+  return (
+    <>
+      {...loadedResults.map((result) => {
+        return (
+          <React.Suspense fallback={<Preloader />}>
+            <ModelPreview name={result.name} tags={result.tags} />
+          </React.Suspense>
+        );
+      })}
+    </>
+  );
+};
